fdb0a050ec0fcea394acb52924eed9742d620d77
kazuki229 <tsuzuku.k@gmail.com>
2018-08-18 01:39:29 +0900

    :art: change color theme

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }

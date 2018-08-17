791ee23334d5777ee563d188dd958346e917585e
kazuki229 <tsuzuku.k@gmail.com>
2018-08-18 01:14:52 +0900

    :sparkles: add hearts

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
   final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
@@ -61,10 +62,15 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
